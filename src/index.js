4682139831e08f873fa509c07285961ef5eacddf
ambujdubey <ambujdubey89@gmail.com>
2020-11-30 13:56:05 +0530

    conditional rendering

--- src/index.js
+++ src/index.js
@@ -10,11 +10,11 @@ class App extends React.Component {
     this.state = { lat: null, errorMessage: '' };
 
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
+      position => {
         // we called setstate!!!!
         this.setState({ lat: position.coords.latitude });
       },
-      (err) => {
+      err => {
         this.setState({ errorMessage: err.message });
       }
     );
@@ -22,13 +22,15 @@ class App extends React.Component {
 
   // React says we have to define render!!
   render() {
-    return (
-      <div>
-        Latitude: {this.state.lat}
-        <br />
-        Error: {this.state.errorMessage}
-      </div>
-    );
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+
+    return <div>Loading!</div>;
   }
 }
 
