9bd8935f17d13ab4f7804ddaabdbca7467ce1f3c
ambujdubey <ambujdubey89@gmail.com>
2020-11-30 09:41:21 +0530

    start work on session app

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
 
 const App = () => {
-  return (
-    <div className="ui container comments">
-      <CommentDetail author="Sam" timeAgo="Today at 4:45PM" avatar="https://source.unsplash.com/random" />
-      <CommentDetail author="Alex" timeAgo="Today at 2:00AM" avatar="https://source.unsplash.com/random" />
-      <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" avatar="https://source.unsplash.com/random" />
-    </div>
-  );
+  return <div>Hi there!</div>;
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
