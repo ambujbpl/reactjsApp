5d7b9b80f4ba603514e4f321cb1b368a4168db66
ambujdubey <ambujdubey89@gmail.com>
2020-12-02 06:21:33 +0530

    add onfor submit prevent Default behaviour

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,20 +1,25 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
-  onInputChange(event) {
-    console.log(event.target.value);
+  state = { term: '' };
+
+  onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(this.state.term);
   }
 
   render() {
     return (
       <div className="ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" onChange={this.onInputChange} />
-            
-            <label>Name Search</label>
-            <input type="text" onChange={(event) => { console.log('event : ',event.target.value) }} />
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
           </div>
         </form>
       </div>
