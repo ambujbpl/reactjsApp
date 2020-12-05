4165cd70425a6e78bc2d2fd609a83eec0fb6a26d
ambujdubey <ambujdubey89@gmail.com>
2020-12-05 06:43:25 +0530

    Add nvideolist component

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+
+const KEY = 'AIzaSyCN6e98GjAfa_d4E4mISSn8uCP6g5b1DvE';
 
 class App extends React.Component {
   state = { videos: [] };
@@ -11,14 +14,15 @@ class App extends React.Component {
         q: term
       },
     });
+
     this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />I have{' '}
-        {this.state.videos.length} videos.
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
