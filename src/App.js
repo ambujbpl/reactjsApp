b1de13b90178433008ebf298991adaced31545cd
ambujdubey <ambujdubey89@gmail.com>
2020-12-06 11:49:49 +0530

    add translator component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
 
 const items = [
   {
@@ -34,16 +35,9 @@ const options = [
 ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-
   return (
     <div>
-      <Dropdown
-        label='Select a Color'
-        selected={selected}
-        onSelectedChange={setSelected}
-        options={options}
-      />
+      <Translate />
     </div>
   );
 };
